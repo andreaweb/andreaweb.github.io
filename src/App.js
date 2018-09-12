31e37264217b3a5ec5a00411469f898461c44fa4
andreaweb <deiainluv@gmail.com>
2018-09-12 09:41:06 -0300

    starting new design for portfolio

--- src/App.js
+++ src/App.js
@@ -47,20 +47,41 @@ class App extends Component {
     </div>
   </header>
   <main class="main light-orange-bg">
-    <aside class="aside dark-blue-bg white">
-      <h5>
-      I'm searching for a new job at a place that: 
-      <ol>
-        <li>Has technical challenges and a fast pace</li>
-        <li>Cares more about actual deliver and performance than appearances</li>
-        <li>Offers a good work environment that help you focus and/or 
-          is an adept of home office
-        </li>
-      </ol> 
-      If your company fits the description and you like my portfolio, 
-      please message me!
-      </h5>
-      <section class="contact">
+  
+      
+      
+<div class="portfolio">
+      {this.state.projects.map(
+          (project, key) =>
+          <section class="project" key={key}>
+                <a 
+                  className="project__link"
+                  style={{backgroundImage: `url(${project.thumbnail})`}} 
+                  href={project.link} 
+                  title={project.name} 
+                  rel="noopener noreferrer" 
+                  target="_blank"
+                >
+                  
+                </a>
+              <h3>{project.name} <i class="fa fa-plus" /></h3>
+              <span>{project.stack.map(
+                (stack, i) => 
+                <span className="stack" key={i}>{stack}</span>
+                )}
+              </span>
+               <p class="description display-none">
+                  {project.description}
+                </p>
+            
+          </section>
+        )
+      }
+      
+    </div>
+  </main>
+
+  <footer class="contact">
         <a 
           href="https://www.linkedin.com/in/andreafrontend/?locale=en_US" 
           title="LinkedIn" 
@@ -87,31 +108,7 @@ class App extends Component {
         >
           <i class="fa fa-envelope white"></i>
         </a>
-      </section>
-    </aside>
-<div class="portfolio">
-      {this.state.projects.map(
-          (project, key) =>
-          <section class="project" key={key}>
-            <div class="title">
-              <h3>{project.name}</h3>
-               <p class="description">
-                  {project.description}
-                  {project.stack.map((stack, i) => <span class="tag skills" key={i}>{stack}</span>)}
-                  {project.occasion ? <span class="tag occasion">{project.occasion}</span> : null }
-                </p>
-            </div>
-            <div class="card">
-              <a href={project.link} title={project.name} rel="noopener noreferrer" target="_blank">
-                <img class="card__image" alt="" src={project.thumbnail} />
-              </a>
-            </div>
-          </section>
-        )
-      }
-      
-    </div>
-  </main>
+      </footer>
       </div>
     );
   }
