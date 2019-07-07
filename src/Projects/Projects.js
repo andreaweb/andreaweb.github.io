dc95bb9c047b0db50a1f0ec5a7333bedd54f043c
Andrea Santana <return.santana@gmail.com>
2019-07-07 11:37:54 +0200

    Projects using hooks and Summary stateless

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,130 +1,111 @@
-import React from 'react';
+import React, { useState } from 'react';
 import projects from './projects.json';
 
-class Projects extends React.Component{
-	//const [projectsArr, setProjectsArr] = useState(projects);
-	constructor(props){
-		super(props);
-		this.state = {
-			projectsArr: projects,
-			refsArr: [],
-			activeDescription: null,
-			search: [
-		        //"Jquery", 
-		        "api", 
-		        "css animations",
-		        "html5 canvas",
-		        "service worker",
-		        "accessibility",
-		        "javascript",
-		        "react",
-		        "css grid",
-		        "redux"
-	        ]
-		}
+function Projects(props){
+	const [projectsArr, setProjectsArr] = useState(projects);
+	const [activeDescription, setActiveDescription] = useState(null);
+	const refsArr = [];
+	
+	// componentDidMount(){
+	// 	let filteredArr = this.state.projectsArr
+	//     this.setState({
+	//       projectsArr: filteredArr.filter(
+	//         project =>    
+	//           project.stack.map(
+	//             (stack) => 
+	//             this.state.search.includes(stack.toLowerCase())
+	//           ).reduce(
+	//             (prev, next) =>
+	//             prev === true || next === true ? true : false
+	//           )
+	//       )
+	//     })   
+	// }
+	const addProject = (project) => {
+		refsArr.push(project)
 	}
-	componentDidMount(){
-		let filteredArr = this.state.projectsArr
-	    this.setState({
-	      projectsArr: filteredArr.filter(
-	        project =>    
-	          project.stack.map(
-	            (stack) => 
-	            this.state.search.includes(stack.toLowerCase())
-	          ).reduce(
-	            (prev, next) =>
-	            prev === true || next === true ? true : false
-	          )
-	      )
-	    })   
-	}
-	addProject = (project) => {
-		this.state.refsArr.push(project)
-	}
-	toggleDescription = (e,key) => {
-		const current = this.state.activeDescription;
+	const toggleDescription = (e,key) => {
+		const current = activeDescription;
 		if(current === key){
-		  this.setState({activeDescription: null})
+		  setActiveDescription(null)
 		}else{
-		  this.setState({activeDescription: key})
+		  setActiveDescription(key)
 		}
 	}
-	render(){
-		return(
-			<main className="portfolio">
-		    {//<input FILTER IS CURRENTLY NOT SO USEFUL... THERE'S NOT THAT MANY PROJECTS!
-			   // ref={input => this.search = input}
-			   // onChange={this.chooseStack} 
-			///>
-			}
-		      {this.state.projectsArr.map(
-		          (project, key) =>
-		          <section 
+
+	return(
+		<main className="portfolio">
+	    {//<input FILTER IS CURRENTLY NOT SO USEFUL... THERE'S NOT THAT MANY PROJECTS!
+		   // ref={input => this.search = input}
+		   // onChange={this.chooseStack} 
+		///>
+		}
+	      {
+	      	projectsArr.map(
+	           (project, key) =>
+	            <section 
 		            className={
-		              this.state.activeDescription===key
+		              activeDescription===key
 		              ? "project active"
 		              : "project"
 		            }
 		            key={project.name} 
 		            id={key} 
-		            ref={ this.addProject }
-		          >
-		              <a 
+		            ref={ addProject }
+		        >
+		      		<a
 		                className="project__link"
 		                style={{backgroundImage: `url(${project.thumbnail})`}} 
 		                href={project.link} 
 		                title={
-		                  this.props.currentLanguage === "EN" 
+		                  props.currentLanguage === "EN" 
 		                  ? project.name
-		                  : this.props.currentLanguage === "PT" ? project.nome : project.titel
+		                  : props.currentLanguage === "PT" ? project.nome : project.titel
 		                } 
 		                rel="noopener noreferrer" 
 		                target="_blank"
-		              >
-		              </a>
-		              <h3 className="project__title">
-		                {
-		                  this.props.currentLanguage === "EN" 
-		                  ? project.name
-		                  : this.props.currentLanguage === "PT" ? project.nome : project.titel
-		                } 
+		            >
+		            </a>
+	               	<h3 className="project__title">
+		               {
+		     			props.currentLanguage === "EN" 
+		      				? project.name
+		      				: props.currentLanguage === "PT" ? project.nome : project.titel
+		               } 
 		                <i 
 		                  role="link" 
-		                  onClick={(e) => this.toggleDescription(e,key)}
+		                  onClick={(e) => toggleDescription(e,key)}
 		                  tabIndex="0" 
 		                  className="fa fa-plus project__icon" 
 		                />
-		              </h3>
-		              <span className="project__stack">
-		                {project.stack.map(
-		                  (stack, i) => 
-		                  <span className="project__stack__item" key={i}>{stack}</span>
-		                  )
-		                }
-		              </span>
-		              <p 
-		                className={ 
-		                 this.state.activeDescription===key
-		                  ? "project__description project__description--open" 
-		                  : "project__description"
-		                }
-		              >
-		              
-		                {
-		                  this.props.currentLanguage === "EN" 
-		                  ? project.description
-		                  : this.props.currentLanguage === "PT" 
-		                  	? project.descricao : project.Omschrijving
-		                } 
-		              
-		              </p>
-		            
-		          </section>
-		        )
-		      }
-		      
-		    </main>
-		)
-	}
+	               </h3>
+	               <span className="project__stack">
+	                {project.stack.map(
+	                  (stack, i) => 
+	                  <span className="project__stack__item" key={i}>{stack}</span>
+	                  )
+	                }
+	              </span>
+	              <p 
+	                className={ 
+	                 activeDescription===key
+	                  ? "project__description project__description--open" 
+	                  : "project__description"
+	                }
+	              >
+	              
+	                {
+	                  props.currentLanguage === "EN" 
+	                  ? project.description
+	                  : props.currentLanguage === "PT" 
+	                  	? project.descricao : project.Omschrijving
+	                } 
+	              
+	              </p>
+	      		</section>
+	        )
+	      }     
+	    </main>
+	)
 }
 export default Projects
--- src/Summary/Summary.js
+++ src/Summary/Summary.js
@@ -2,70 +2,67 @@ import React from 'react';
 import summary from './summary.json';
 import Button from './Button';
 
-class Summary extends React.Component{
-	render(){
-		return(
-			<section className="summary">
-			    <div className="summary__text">
-			      { 
-			        this.props.currentLanguage === "EN"
-			        &&
-			          summary[0].textLines.map(
-			            (text) =>
-			            <p key={text}>{text}</p>
-			          )
-			      }
-			      { 
-			        this.props.currentLanguage === "PT"
-			        &&
-			          summary[1].textLines.map(
-			            (text) =>
-			            <p key={text}>{text}</p>
-			          )
-			      }
-			      { 
-			        this.props.currentLanguage === "NL"
-			        &&
-			          summary[2].textLines.map(
-			            (text) =>
-			            <p key={text}>{text}</p>
-			          )
-			      }
-			    </div>
-				<div className="contact">
-					<Button />
-			        <a 
-			          href="https://www.linkedin.com/in/andreafrontend/?locale=en_US" 
-			          title="LinkedIn" 
-			          rel="noopener noreferrer" 
-			          target="_blank"
-			          className="contact__link"
-			        >
-			          <i className="fa fa-linkedin-square white"></i>
-			        </a>
+const Summary = (props) => (
+		<section className="summary">
+		    <div className="summary__text">
+		      { 
+		        props.currentLanguage === "EN"
+		        &&
+		          summary[0].textLines.map(
+		            (text) =>
+		            <p key={text}>{text}</p>
+		          )
+		      }
+		      { 
+		        props.currentLanguage === "PT"
+		        &&
+		          summary[1].textLines.map(
+		            (text) =>
+		            <p key={text}>{text}</p>
+		          )
+		      }
+		      { 
+		        props.currentLanguage === "NL"
+		        &&
+		          summary[2].textLines.map(
+		            (text) =>
+		            <p key={text}>{text}</p>
+		          )
+		      }
+		    </div>
+			<div className="contact">
+				<Button />
+		        <a 
+		          href="https://www.linkedin.com/in/andreafrontend/?locale=en_US" 
+		          title="LinkedIn" 
+		          rel="noopener noreferrer" 
+		          target="_blank"
+		          className="contact__link"
+		        >
+		          <i className="fa fa-linkedin-square white"></i>
+		        </a>
 
-			        <a 
-			          href="https://twitter.com/andreafrontend?lang=en" 
-			          title="Twitter" 
-			          rel="noopener noreferrer" 
-			          target="_blank"
-			          className="contact__link"
-			        >
-			          <i className="fa fa-twitter-square white"></i>
-			        </a>
+		        <a 
+		          href="https://twitter.com/andreafrontend?lang=en" 
+		          title="Twitter" 
+		          rel="noopener noreferrer" 
+		          target="_blank"
+		          className="contact__link"
+		        >
+		          <i className="fa fa-twitter-square white"></i>
+		        </a>
 
-			        <a 
-			          href="mailto:return.santana@gmail.com" 
-			          title="E-mail" 
-			          rel="noopener noreferrer" 
-			          target="_blank"
-			          className="contact__link"
-			        >
-			          <i className="fa fa-envelope white"></i>
-			        </a>
-			    </div>
-			</section>
-		)
-	}
-}
+		        <a 
+		          href="mailto:return.santana@gmail.com" 
+		          title="E-mail" 
+		          rel="noopener noreferrer" 
+		          target="_blank"
+		          className="contact__link"
+		        >
+		          <i className="fa fa-envelope white"></i>
+		        </a>
+		    </div>
+		</section>
+)
+
 export default Summary
